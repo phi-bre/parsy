1317a3a3d1ba6ee703c6f4884a66b823269dc302
Philip Breuer <philip.breuer@pm.me>
2020-04-04 01:04:32 +0200

    Play around with regex

--- test/parsy.test.ts
+++ test/parsy.test.ts
@@ -45,22 +45,22 @@ describe('#parsy', () => {
         'multiplication: ' + Object.keys(star).join(' ')
     );
 
-    const input = new Array(5_000_000).fill(1).join('');
+    const input = new Array(10_000_000).fill(1).join('');
 
     console.time('regex');
     const [group] = /^[0-9]+/.exec(input) || [];
     console.timeEnd('regex');
 
     console.time('while');
+    const pattern = /^[0-9]+/;
     let test = '', length = 0;
-    while (length < input.length) {
-        test += input[length++];
+    while (length++ < input.length) {
+        // pattern.exec(input.substring(0, length));
     }
     console.timeEnd('while');
 
     console.time('lookup');
-    let index = 0;
-    let string = '';
+    let index = 0, last = 0;
     let tree: any = [];
     tree.type = value; // start
 
@@ -69,7 +69,7 @@ describe('#parsy', () => {
             case undefined:
             case null:
                 // console.log('close scope: ' + tree.type + ' "' + input[index] + '" @ ' + index);
-                tree.push(string);
+                tree.push(input.substring(last, index));
                 if (tree = tree.parent) continue;
                 else throw 'unexpected token: "' + input[index] + '" @ ' + index;
             default:
@@ -79,14 +79,14 @@ describe('#parsy', () => {
                 node.parent = tree;
                 tree.push(node);
                 tree = node;
+                last = index;
             case tree.type:
                 // console.log('continue scope: ' + next[type] + ' "' + input[index] + '" @ ' + index);
-                string += input[index];
                 index++;
         }
     }
 
-    tree.push(string);
+    tree.push(input.substring(last, index));
     tree = tree.parent;
 
     if (tree.parent) {
@@ -99,4 +99,102 @@ describe('#parsy', () => {
         // console.log(group);
         expect(true).toBeTruthy();
     });
+});
+
+describe('#two', function () {
+    // object: [A-Za-z0-9]+ '{' object* '}'
+    const input = `
+            grammar {
+                test {
+            
+                }
+                abc {}
+            }
+        `;
+
+    const sequence = (...rules) => [seq, ...rules];
+    const alternation = (...rules) => [alt, ...rules];
+    const alias = (type: string, ...rules) => [type, ...rules];
+
+    const ter = Symbol('terminal');
+    const alt = Symbol('alternation');
+    const seq = Symbol('sequence');
+    const opt = Symbol('optional');
+    const sco = Symbol('scope');
+
+    type Rule = (Array<string> | Function) & { label?: string, op: Function };
+    type Node = Array<string | Node> & { label: string };
+
+    // remove start symbol
+    // object: id '{' object* '}'
+    // id: [A-Za-z0-9]+
+
+    // remove enclosed terminals
+    // object: id so object* sc
+    // id: [A-Za-z0-9]+
+    // so: '{'
+    // sc: '}'
+
+    // break up recursions
+    // object: id so obj_rec sc
+    // id: [A-Za-z0-9]+
+    // so: '{'
+    // sc: '}'
+    // obj_rec: object obj_rec
+
+    const object: any[] = [opt, /^[ \n\t]*([A-Za-z0-9]+)[ \n\t]*({)/g, /^[ \n\t]*(})/g];
+    object.push(object);
+
+    let index = 0;
+    let tree = [] as any;
+    tree.type = object;
+    do {
+        const [op, open, close, type] = tree.type;
+        open.lastIndex = index;
+        const match = open.exec(input);
+        if (match) {
+            tree.push(...match.slice(1));
+            console.log('open: ' + match[1]);
+            const node = [] as any;
+            node.type = type;
+            node.parent = tree;
+            tree = node;
+            index += match[0].length;
+        } else {
+            close.lastIndex = index;
+            const match = close.exec(input);
+            if (!match) throw 'blub';
+            console.log('close: ' + match[1]);
+            index += match[0].length;
+            tree.push(...match.slice(1));
+            tree.parent.push(tree);
+            tree = tree.parent;
+        }
+    } while (tree.parent && index < input.length);
+
+
+    tree.parent.push(tree);
+    tree = tree.parent;
+
+    console.log(tree);
+
+    it('should ', function () {
+        expect(true).toBeTruthy();
+    });
+
+    // const id = [ter, /^[A-Za-z0-9]+/];
+    // let obj = [seq];
+    // const bo = [ter, /^{/];
+    // const bc = [ter, /^}/];
+    // const id_bo = [seq, id, bo];
+    // const id_bo_obj = [seq, id_bo];
+    // const obj_opt = [alt, obj, null];
+    // const obj_rec = [seq, obj, obj];
+    // // obj.push(seq, id_bo_obj);
+    //
+    // let tree: Node = [] as any;
+    // tree.label = value; // start
+    // for (let i = 0; i < input.length; i++) {
+    //
+    // }
 });
